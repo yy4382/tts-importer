8cdb4f2eeb5cb9e7312d9f2d01f6eb161aa6cc44
Chris Yang <yy4382@outlook.com>
2024-01-08 16:41:23 +0800

    feat: update metadata

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({  link: [//网站link，可以在这里引入css和icon等
+    { rel: 'shortcut icon', href: '/favicon.png' },
+    { rel: 'apple-touch-icon', href: '/favicon.png' }
+  ]
+})
+useSeoMeta({
+  title: 'Azure TTS Importer',
+  ogTitle: 'Azure TTS Importer',
+  description: '轻松将微软 Azure TTS 导入到小说软件',
+  ogDescription: '轻松将微软 Azure TTS 导入到小说软件',
+  ogImage: '/favicon.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
