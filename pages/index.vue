5b3de2ca97fa3efd4ab213294bd84e6ef41b8636
Chris Yang <yy4382@outlook.com>
2024-01-13 13:13:42 +0800

    fix: button position

--- pages/index.vue
+++ pages/index.vue
@@ -76,15 +76,18 @@
             </div>
         </Placement>
         <Placement v-if="voiceList && vconfig.voice" class="mb-4">
-            <h2 class="text-4xl font-extrabold dark:text-white">试听
-                <label class="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" v-model="useTest" class="sr-only peer" @click="useTest = !useTest">
-                    <div
-                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-                    </div>
-                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-white">{{ useTest ? "隐藏" : "显示" }}</span>
-                </label>
-            </h2>
+            <div class="flex justify-between">
+                <h2 class="text-4xl font-extrabold dark:text-white">试听</h2>
+                <div class="inline-flex items-center">
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" v-model="useTest" class="sr-only peer" @click="useTest = !useTest">
+                        <div
+                            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                        </div>
+                        <span class="ml-3 text-sm font-medium text-gray-900 dark:text-white">{{ useTest ? "隐藏" : "显示" }}</span>
+                    </label>
+                </div>
+            </div>
             <div v-if="useTest">
                 <textarea id="message" rows="4" v-model="testText"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-4"
