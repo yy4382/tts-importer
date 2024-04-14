8a7fd3b05770b45174bee7a3f99e3dfe06a290c1
Chris Yang <yy4382@outlook.com>
2024-04-14 11:33:56 +0800

    fix: speak style issue

--- pages/index.vue
+++ pages/index.vue
@@ -252,7 +252,7 @@ function genSSML(config: VoiceConfig, text: string) {
         return
     const pitch = config.pitch === "default" ? null : config.pitch;
     const ssml =
-        `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">` +
+        `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="zh-CN">` +
         `<voice name="${config.voice.ShortName}">` +
         `${pitch ? `<prosody pitch="${config.pitch}">` : ""}` +
         `${config.useStyle ? `<mstts:express-as style="${config.style}">` : ""}` +
