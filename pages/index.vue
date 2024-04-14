bd362cf21f8afc9690037c599b15fc790df34b4a
Chris Yang <yy4382@outlook.com>
2024-04-14 11:40:28 +0800

    fix: disabled state of useVoiceStyle checkbox

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
                     </div>
                     <div class="mb-8">
                         <div class="flex items-center mb-4">
-                            <input id="useVoiceStyle" v-model="vconfig.useStyle" type="checkbox" :disabled="!selVoiceStyle"
+                            <input id="useVoiceStyle" v-model="vconfig.useStyle" type="checkbox" :disabled="selVoiceStyle.length === 0"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                             <label v-if="selVoiceStyle" for="useVoiceStyle"
                                 class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">使用声音风格</label>
