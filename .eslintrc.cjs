642bedc5913358c1982b835dd9fe30fd2e488b60
Chris Yang <yy4382@outlook.com>
2024-05-20 02:33:15 +0800

    chore(deps): remove old eslint config and deps, add pinia deps

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
-  rules: {
-    "vue/first-attribute-linebreak": "off",
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-    "no-alert": "warn",
-  },
-};
