53053cdc5ac96fcc2e57f0d22243e379fa1912d7
Chris Yang <yy4382@outlook.com>
2024-05-20 11:21:08 +0800

    refactor: Add active state to help page link in NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -22,6 +22,7 @@
       </ULink>
       <ULink
         to="/help"
+        :active="isHelpPage"
         active-class="text-primary"
         inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
       >
@@ -39,3 +40,7 @@
     </div>
   </nav>
 </template>
+<script setup>
+const route = useRoute();
+const isHelpPage = computed(() => route.path.startsWith("/help"));
+</script>
