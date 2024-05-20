ae5c8f734e71e6762afd8b3d1f2d59bc4b39bab6
Chris Yang <yy4382@outlook.com>
2024-05-20 10:41:18 +0800

    chore: Update tsconfig.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./components/**/*.{js,vue,ts}",
+  "./layouts/**/*.vue",
+  "./pages/**/*.vue",
+  "./plugins/**/*.{js,ts}",
+  "./app.vue",
+  "./error.vue",
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
