aaf7bcb14db5eecbc641be0b355f0089e52c300b
Chris Yang <yy4382@outlook.com>
2025-02-20 19:51:44 +0800

    fix: wrap ApiUrl component in Suspense for improved loading handling
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/app/(ra)/ra/page.tsx
+++ src/app/(ra)/ra/page.tsx
@@ -1,13 +1,16 @@
 import { AppTopbar } from "@/components/modules/app-topbar";
 import ApiUrl from "../api-url";
 import { SynthesisForm } from "../synthesis-form";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="w-full">
       <AppTopbar location={[{ title: "面板" }, { title: "Edge TTS 导入" }]} />
       <div className="flex flex-col lg:flex-row px-4 lg:px-0 w-fit flex-grow gap-4 mx-auto my-4">
-        <ApiUrl />
+        <Suspense>
+          <ApiUrl />
+        </Suspense>
         <SynthesisForm />
       </div>
     </div>
