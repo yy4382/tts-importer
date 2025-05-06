5797ccb8cfee6e5eda73a4c2c3190b1b85ddc0ff
Chris Yang <yy4382@outlook.com>
2025-05-06 14:18:08 +0800

    chore: disable posthog search params recording

--- src/components/utils/posthog-provider.tsx
+++ src/components/utils/posthog-provider.tsx
@@ -1,48 +1,46 @@
 // app/providers.tsx
-'use client'
+"use client";
 
-import { usePathname, useSearchParams } from "next/navigation"
-import { useEffect, Suspense } from "react"
-import { usePostHog } from 'posthog-js/react'
+import { usePathname } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from "posthog-js/react";
 
-import posthog from 'posthog-js'
-import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
-    })
-  }, [])
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true,
+    });
+  }, []);
 
   return (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
-  )
+  );
 }
 
 function PostHogPageView() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthog = usePostHog()
+  const pathname = usePathname();
+  const posthog = usePostHog();
 
   // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname
-      if (searchParams.toString()) {
-        url = url + "?" + searchParams.toString();
-      }
+      const url = window.origin + pathname;
 
-      posthog.capture('$pageview', { '$current_url': url })
+      posthog.capture("$pageview", { $current_url: url });
     }
-  }, [pathname, searchParams, posthog])
+  }, [pathname, posthog]);
 
-  return null
+  return null;
 }
 
 // Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
@@ -53,5 +51,5 @@ function SuspendedPostHogPageView() {
     <Suspense fallback={null}>
       <PostHogPageView />
     </Suspense>
-  )
+  );
 }
