43416a18435fa9774b901bce6c89c8096f1221ce
Chris Yang <yy4382@outlook.com>
2025-09-25 21:58:57 +0800

    feat(ra): refactor export functionality to use tabs and improve state parsing

--- src/app/ra/export.tsx
+++ src/app/ra/export.tsx
@@ -4,8 +4,11 @@ import { Button } from "@/components/ui/button";
 import { useAtomValue } from "jotai";
 import { toast } from "sonner";
 import {
+  RaApiConfig,
   raApiConfigAtom,
+  RaState,
   raStateSchema,
+  RaVoiceConfig,
   validRaVoiceConfigAtom,
 } from "./ra-data";
 import {
@@ -14,6 +17,8 @@ import {
 } from "@/app/ra/generate-profile";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { posthog } from "posthog-js";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 function copyToClipboard(text: string) {
   navigator.clipboard
@@ -37,84 +42,165 @@ function downloadFile(text: string, filename: string) {
   toast("已开始下载配置文件，请使用软件的“本地导入”功能导入配置");
 }
 
-export function ExportRa() {
-  const voiceConfig = useAtomValue(validRaVoiceConfigAtom);
-  const api = useAtomValue(raApiConfigAtom);
-
-  function onSubmit(type: "legado" | "ireadnote" | "sourcereader") {
-    if (!api.url) {
-      toast("API URL 未设置");
-      return;
-    }
-    if (!voiceConfig) {
-      toast("语音配置未设置");
-      return;
-    }
-
-    const raState = raStateSchema.safeParse({ api, voice: voiceConfig });
-    if (!raState.success) {
-      toast("生成配置失败", {
-        description: raState.error.issues
-          .map((issue) => issue.message)
-          .join(", "),
-      });
-      return;
-    }
-
-    const result = (() => {
-      switch (type) {
-        case "legado":
-          return generateProfileLegado(raState.data);
-        case "ireadnote":
-          return generateProfileIreadnote(raState.data);
-        case "sourcereader":
-          return generateProfileLegado(raState.data);
-      }
-    })();
-
-    posthog?.capture("profile exported", {
-      type: "ra",
-      app: type,
-      method: "copy-profile",
-    });
-
-    switch (type) {
-      case "ireadnote":
-        copyToClipboard(result);
-        break;
-      case "legado":
-        if (voiceConfig.voiceName.type === "single") {
-          copyToClipboard(result);
-        } else {
-          downloadFile(
-            result,
-            `ra-profile-${type}-${voiceConfig.voiceName.type}.json`
-          );
-        }
-        break;
-      case "sourcereader":
-        downloadFile(
-          result,
-          `ra-profile-${type}-${
-            voiceConfig.voiceName.type === "single"
-              ? voiceConfig.voiceName.name
-              : voiceConfig.voiceName.type
-          }.json`
-        );
-        break;
-    }
+function parseRaState(
+  api: RaApiConfig,
+  voiceConfig: RaVoiceConfig | null
+): RaState | undefined {
+  if (!api.url) {
+    toast("API URL 未设置");
+    return;
   }
+  if (!voiceConfig) {
+    toast("语音配置未设置");
+    return;
+  }
+  const appState = raStateSchema.safeParse({ api, voice: voiceConfig });
+  if (!appState.success) {
+    toast("生成配置失败", {
+      description: appState.error.issues
+        .map((issue) => issue.message)
+        .join(", "),
+    });
+    return;
+  }
+  return appState.data;
+}
 
+function capture(type: "legado" | "ireadnote" | "sourcereader") {
+  posthog?.capture("profile exported", {
+    type: "ra",
+    app: type,
+    method: "copy-profile",
+  });
+}
+
+export function ExportRa() {
   return (
     <Card className="w-card">
       <CardHeader>
         <CardTitle>导出配置</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-row gap-2 flex-wrap">
-        <Button onClick={() => onSubmit("legado")}>阅读配置</Button>
-        <Button onClick={() => onSubmit("ireadnote")}>爱阅记配置</Button>
-        <Button onClick={() => onSubmit("sourcereader")}>源阅读配置</Button>
+        <Tabs defaultValue="legado">
+          <TabsList className="-ml-1">
+            <TabsTrigger value="legado">阅读</TabsTrigger>
+            <TabsTrigger value="ireadnote">爱阅记</TabsTrigger>
+            <TabsTrigger value="sourcereader">源阅读</TabsTrigger>
+          </TabsList>
+          <TabsContent value="legado">
+            <LegadoPanel />
+          </TabsContent>
+          <TabsContent value="ireadnote">
+            <IreadnotePanel />
+          </TabsContent>
+          <TabsContent value="sourcereader">
+            <SourcereaderPanel />
+          </TabsContent>
+        </Tabs>
       </CardContent>
     </Card>
+  );
+}
+
+function LegadoPanel() {
+  const voiceConfig = useAtomValue(validRaVoiceConfigAtom);
+  const api = useAtomValue(raApiConfigAtom);
+
+  function onExport() {
+    const raState = parseRaState(api, voiceConfig);
+    if (!raState) return;
+
+    const result = generateProfileLegado(raState);
+
+    capture("legado");
+
+    if (raState.voice.voiceName.type === "single") {
+      copyToClipboard(result);
+    } else {
+      downloadFile(
+        result,
+        `ra-profile-legado-${raState.voice.voiceName.type}.json`
+      );
+    }
+  }
+
+  return (
+    <div>
+      <Button disabled={voiceConfig === null} onClick={onExport}>
+        {voiceConfig?.voiceName.type === "single" ? "复制" : "下载"}阅读配置
+      </Button>
+      <div className="prose dark:prose-invert prose-sm mt-2 prose-p:my-1">
+        <p>
+          对于单个语音，复制配置后在「朗读引擎」界面先点击加号，再点击弹出窗口中三个点下拉菜单中的「粘贴源」
+        </p>
+        <p>
+          对于多个语音，下载配置后在「朗读引擎」界面直接点三个点，选择本地导入即可。
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function IreadnotePanel() {
+  const voiceConfig = useAtomValue(validRaVoiceConfigAtom);
+  const api = useAtomValue(raApiConfigAtom);
+
+  function onExport() {
+    const raState = parseRaState(api, voiceConfig);
+    if (!raState) return;
+
+    const result = generateProfileIreadnote(raState);
+
+    capture("ireadnote");
+
+    copyToClipboard(result);
+  }
+
+  return (
+    <div>
+      <Button disabled={voiceConfig === null} onClick={onExport}>
+        复制爱阅记配置
+      </Button>
+      <div className="prose dark:prose-invert prose-sm mt-2 prose-p:my-1">
+        <p>
+          在线语音库管理 - 右上角三点 - JSON 输入 中粘贴即可。
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function SourcereaderPanel() {
+  const voiceConfig = useAtomValue(validRaVoiceConfigAtom);
+  const api = useAtomValue(raApiConfigAtom);
+
+  function onExport() {
+    const raState = parseRaState(api, voiceConfig);
+    if (!raState) return;
+
+    const result = generateProfileLegado(raState);
+
+    capture("sourcereader");
+
+    const filename = `ra-profile-sourcereader-${
+      raState.voice.voiceName.type === "single"
+        ? raState.voice.voiceName.name
+        : raState.voice.voiceName.type
+    }.json`;
+
+    downloadFile(result, filename);
+  }
+
+  return (
+    <div>
+      <Button disabled={voiceConfig === null} onClick={onExport}>
+        下载源阅读配置
+      </Button>
+      <div className="prose dark:prose-invert prose-sm mt-2 prose-p:my-1">
+        <p>
+          使用「本地导入」功能导入即可。
+        </p>
+      </div>
+    </div>
   );
 }
