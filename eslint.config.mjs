50a1c7e2c811fa2b84309b35dc3ab4b1c310d921
Chris Yang <yy4382@outlook.com>
2025-11-14 17:48:10 +0800

    chore: update to nextjs 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  {
-    ignores: [".content-collections"],
+const eslintConfig = [{
+  ignores: [".content-collections"],
+}, ...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-    },
-  },
-];
+}];
 
 export default eslintConfig;
